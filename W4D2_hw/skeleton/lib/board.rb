f3998f8c50036d36b2d6cbc3f36b486f1a89d908
ianverger <ian.verger@gmail.com>
2022-08-08 22:25:53 -0400

    w4d2

--- W4D2_hw/skeleton/lib/board.rb
+++ W4D2_hw/skeleton/lib/board.rb
@@ -2,20 +2,57 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+      @name1 = name1
+      @name2 = name2
+      @cups = Array.new(14) {Array.new}
+      place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups[0..5].each { |cup| cup.push(:stone, :stone, :stone, :stone) }
+    @cups[7..12].each { |cup| cup.push(:stone, :stone, :stone, :stone) }
   end
 
   def valid_move?(start_pos)
+    if start_pos < 0 || start_pos > 12
+      raise 'Invalid starting cup'
+    elsif @cups[start_pos].empty?
+      raise 'Starting cup is empty'
+    end
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
+
+      render
+      next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
